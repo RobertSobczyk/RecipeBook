909bc430b40b684571707a2f6cae1a512742a97a
Robert Sobczyk <szopkowy@gmail.com>
2018-07-19 21:01:21 +0200

    Add Recipe List

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    // tslint:disable-next-line:max-line-length
+    new Recipe('Jajecznica', 'Danie z jajek i dodatków', 'https://img1.cookinglight.timeinc.net/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2017/01/main/creamy-soft-scrambled-eggs.jpg?itok=UHRuIWD_'),
+    // tslint:disable-next-line:max-line-length
+    new Recipe('Warzywa z ryżem', 'Zdrowe i tanie danie', 'https://www.bbcgoodfood.com/sites/default/files/recipe_images/stirfrychicken_5795.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
