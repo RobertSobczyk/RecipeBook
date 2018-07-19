0b6f15a30fc4add095625225830a47e2a9d5f907
Robert Sobczyk <szopkowy@gmail.com>
2018-07-19 18:32:51 +0200

    Add shopping-list.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       HeaderComponent,
-      RecipesComponent
+      RecipesComponent,
+      ShoppingListComponent
    ],
    imports: [
       BrowserModule
